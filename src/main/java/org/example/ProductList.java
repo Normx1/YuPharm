0ddc6a74c54d08692f0a56436f696862b11f6e4c
Сергей <Normande2010@mail.ru>
2021-03-15 23:39:57 +0300

    Unit Maven project

--- src/main/java/org/example/ProductList.java
+++ src/main/java/org/example/ProductList.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Map;
 
-public class ProductList implements DrugStorage{
+public class ProductList implements DrugStorage {
     @Override
     public Map<Medicine, Integer> list() {
 

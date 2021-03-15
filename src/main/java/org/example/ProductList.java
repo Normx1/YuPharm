2eb066be9851ca8673944ce89acc95dffb574047
Сергей <Normande2010@mail.ru>
2021-03-15 23:26:58 +0300

    Unit Maven project

---
+++ src/main/java/org/example/ProductList.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Map;
+
+public class ProductList implements DrugStorage{
+    @Override
+    public Map<Medicine, Integer> list() {
+
+        return null;
+    }
+}

7a76aa61abb3a5721a5bc1f0ff49e52f22201092
Yuliya Davydova <vorstellung7@gmail.com>
2021-05-15 20:15:17 +0300

    Fix drug creation in sql drugs

--- src/main/java/com/yu_pharm/model/drug/SqlDrugs.java
+++ src/main/java/com/yu_pharm/model/drug/SqlDrugs.java
@@ -19,7 +19,7 @@ public class SqlDrugs implements Drugs {
 
 	@Override
 	public Drug create() {
-		String sql = "insert into drugs output id values ('')";
+		String sql = "insert into drugs values ('')";
 		try (PreparedStatement st = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 			st.executeUpdate();
 			ResultSet keys = st.getGeneratedKeys();

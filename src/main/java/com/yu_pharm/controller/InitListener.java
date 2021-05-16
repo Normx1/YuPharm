f95040ea8e82f06bc00e4c53faa14a24a053669b
Yuliya Davydova <vorstellung7@gmail.com>
2021-05-16 10:39:07 +0300

    Close db connection on app destroy

--- src/main/java/com/yu_pharm/controller/InitListener.java
+++ src/main/java/com/yu_pharm/controller/InitListener.java
@@ -17,31 +17,54 @@ import java.util.function.Supplier;
 @WebListener
 public class InitListener implements ServletContextListener {
 
+	private final ConnectionSource connection = new ConnectionSource();
+
+
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		try {
-			Supplier<Connection> connection = new Supplier<>() {
-
-				private Connection connection;
-
-				@Override
-				public synchronized Connection get() {
-					try {
-						if (connection == null) {
-							connection = JDBCConnector.getConnection();
-						}
-						return connection;
-					} catch (SQLException ex) {
-						throw new RuntimeException("Failed to get connection to db", ex);
-					}
-				}
-			};
 			Drugs.Smart drugs = new Drugs.Smart(new SqlDrugs(connection));
 			OrderDao<Order> orders = new OrderDao_Imp(drugs);
 			sce.getServletContext().setAttribute("drugs", drugs);
 			sce.getServletContext().setAttribute("orders", orders);
 		} catch (Exception ex) {
 			throw new RuntimeException("Failed to setup application", ex);
+		}
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		connection.close();
+	}
+}
+
+class ConnectionSource implements Supplier<Connection>, AutoCloseable {
+
+	private Connection connection;
+
+
+	@Override
+	public synchronized Connection get() {
+		try {
+			if (connection == null) {
+				connection = JDBCConnector.getConnection();
+			}
+			return connection;
+		} catch (SQLException ex) {
+			throw new RuntimeException("Failed to get connection to db", ex);
+		}
+	}
+
+	@Override
+	public synchronized void close() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException ex) {
+				throw new RuntimeException("Failed to close connection to db", ex);
+			} finally {
+				connection = null;
+			}
 		}
 	}
 }

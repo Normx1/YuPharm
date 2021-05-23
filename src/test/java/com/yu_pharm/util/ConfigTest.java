21267e6b494580ef1c2a9668f754b7b89e1bd2f8
Yuliya Davydova <vorstellung7@gmail.com>
2021-05-23 08:37:19 +0300

    Fix ConfigTest

--- src/test/java/com/yu_pharm/util/ConfigTest.java
+++ src/test/java/com/yu_pharm/util/ConfigTest.java
@@ -8,12 +8,6 @@ class ConfigTest {
 
 	@Test
 	void get() {
-		String key = "hi";
-		String noValue = "12";
-		String value = "test";
-		System.setProperty(key, noValue);
-		assertEquals(noValue, new Config().getValue(key));
-		System.setProperty(key, value);
-		assertEquals(value, new Config().getValue(key));
+		assertEquals("13", new Config().getValue("hi"));
 	}
 }
